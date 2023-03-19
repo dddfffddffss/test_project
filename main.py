fb8239546367ef9145f7faf6dd9926f35493feed
dddfffddffss <dlhwadlhwa@gmail.com>
2023-03-20 07:33:38 +0900

    get_date_stock항상유효함

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 import json
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
 import sub.data as data
 
-with open("data/data.txt",'w') as f:
-    json.dump(data.start_local_data("20230314"), f, ensure_ascii=False, indent=4)
+matplotlib.rcParams['font.family'] ='Malgun Gothic'
+matplotlib.rcParams['axes.unicode_minus'] =False
+
+def make_start_data():
+    with open("data/data.txt",'w') as f:
+        d, startdate, enddate = data.start_local_data()
+        json.dump(d, f, indent=4, ensure_ascii=False)
+
+def get_local_data():
+    return data.get_local_data()
+
+li = []
+data = get_local_data()
+for ticker in data:
+	l = np.array(data[ticker]["거래량"]).astype(int)
+	s = np.std(l)/np.mean(l)
+	if not np.isnan(s):
+		li.append((data[ticker], s, ticker))
+
+li.sort(key=lambda x: x[1])
+for i in li[:30]:
+	print(i[0])
+	print(i[1])
+	print()
+
+l1 = li[-2600:-2610:-1]
+l1.extend(li[-1:-10:-1])
+for i in l1:
+	l = np.array(i[0]["거래량"]).astype(int)
+	plt.plot(l/np.mean(l),label=i[0]["종목명"])
+	print(i[0]["종목명"], i[2], i[0], "\n")
+plt.legend(loc=1)
+plt.show()
--- sub/data.py
+++ sub/data.py
@@ -18,20 +18,39 @@ def get_date_stock(date: str = datetime.datetime.today().strftime("%Y%m%d")) -> 
     otp = requests.post(gen_otp_url, gen_otp_data, headers=headers).text
     down_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
     down_sector_KS  = requests.post(down_url, {'code':otp}, headers=headers)
-    return pd.read_csv(BytesIO(down_sector_KS.content), encoding='EUC-KR')
+    data = pd.read_csv(BytesIO(down_sector_KS.content), encoding='EUC-KR')
+    if pd.isna(data.loc[0,'시가총액']):
+        yasterday = datetime.datetime.strptime(date, "%Y%m%d") - datetime.timedelta(days=1)
+        return get_date_stock(yasterday.strftime("%Y%m%d"))
+    else: 
+        for col in data.columns:
+            data[col] = data[col].astype('str')
+        return data, date
 
 def start_local_data(date=None) -> dict:
     if date==None:
-         current_data = get_date_stock()
+         current_data, date = get_date_stock()
     else:
-         current_data = get_date_stock(date)
+         current_data, date = get_date_stock(date)
+    startdate = date
     grouped = current_data.groupby('종목코드').agg(list)
     result = {
         key: {col: val if col in ['종가', '거래량', '거래대금'] else val[0]
         for col, val in zip(grouped.columns, values)}
         for key, values in zip(grouped.index, grouped.values)
     }
-    return result
+
+    for _ in range(9):
+        yasterday = datetime.datetime.strptime(date, "%Y%m%d") - datetime.timedelta(days=1)
+        current_data, date = get_date_stock(yasterday.strftime("%Y%m%d"))
+        for ticker in list(result):
+            indices = current_data.index[current_data['종목코드'] == ticker].tolist()
+            if not indices:
+                result.pop(ticker, None)
+            else:
+                for category in ['종가', '거래량', '거래대금']:
+                    result[ticker][category].append(current_data.loc[indices[0],category])
+    return result, startdate, date
 
 def get_local_data():
     with open("data\\data.txt",'r') as f:
