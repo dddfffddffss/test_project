30e9adc9ac82b120a48cdb0b7e2a679bb8147e7b
dddfffddffss <dlhwadlhwa@gmail.com>
2023-03-20 23:23:23 +0900

    새로운전략 실험 결과: 새 텍스트 문서

--- main.py
+++ main.py
@@ -1,40 +1,85 @@
+import datetime
 import json
+import traceback
 import numpy as np
+import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
 import sub.data as data
+import sub.pocket as pocket
 
 matplotlib.rcParams['font.family'] ='Malgun Gothic'
 matplotlib.rcParams['axes.unicode_minus'] =False
 
-def make_start_data():
-    with open("data/data.txt",'w') as f:
-        d, startdate, enddate = data.start_local_data()
+wallet, result={}, []
+
+def make_start_data(date):
+    with open("data/"+date+".txt",'w') as f:
+        d, startdate, enddate = data.start_local_data(date)
         json.dump(d, f, indent=4, ensure_ascii=False)
+
+def get_web_data(date, next_timedelta):
+    return data.start_local_data(date, next_timedelta)
 
 def get_local_data():
     return data.get_local_data()
 
-li = []
-data = get_local_data()
-for ticker in data:
-	l = np.array(data[ticker]["거래량"]).astype(int)
-	s = np.std(l)/np.mean(l)
-	if not np.isnan(s):
-		li.append((data[ticker], s, ticker))
+def show_trade_amount():
+	li = []
+	data = get_local_data()
+	for ticker in data:
+		l = np.array(data[ticker]["거래량"]).astype(int)
+		s = np.std(l)/np.mean(l)
+		if not np.isnan(s):
+			li.append((data[ticker], s, ticker))
 
-li.sort(key=lambda x: x[1])
-for i in li[:30]:
-	print(i[0])
-	print(i[1])
-	print()
+	li.sort(key=lambda x: x[1])
+	for i in li[:30]:
+		print(i[0])
+		print(i[1])
+		print()
 
-l1 = li[-2600:-2610:-1]
-l1.extend(li[-1:-10:-1])
-for i in l1:
-	l = np.array(i[0]["거래량"]).astype(int)
-	plt.plot(l/np.mean(l),label=i[0]["종목명"])
-	print(i[0]["종목명"], i[2], i[0], "\n")
-plt.legend(loc=1)
-plt.show()
+	l1 = li[-2600:-2610:-1]
+	l1.extend(li[-1:-10:-1])
+	for i in l1:
+		l = np.array(i[0]["거래량"]).astype(int)
+		plt.plot(l/np.mean(l),label=i[0]["종목명"])
+		print(i[0]["종목명"], i[2], i[0], "\n")
+	plt.legend(loc=1)
+	plt.show()
+
+ssss = []
+try:
+	start_date = "20180301"
+	for i in range(180):
+		current_data, start_date, end_date = get_web_data(start_date, next_timedelta=1)
+		for ticker in current_data:
+			l = np.array(current_data[ticker]["거래량"]).astype(int)
+			m = np.mean(l)
+			if m==0:
+				continue
+			else:
+				indexed_l = l/m
+				if indexed_l[0]>=3 and (ticker in wallet):
+					denominator = wallet.pop(ticker, None)
+					current = int(current_data[ticker]["시가"])
+					result.append(100*(current-denominator)/denominator)
+				elif np.count_nonzero(indexed_l>=3)>=1:
+					pass
+				elif np.count_nonzero((indexed_l[:-1]-indexed_l[1:])<0)>=8 and (ticker not in wallet):
+					wallet[ticker] = int(current_data[ticker]["종가"])
+		men = np.mean(result) if result else 0
+		ssss.append(men)
+		print(start_date, len(result), men)
+		print()
+		start_date = (datetime.datetime.strptime(start_date, "%Y%m%d") + datetime.timedelta(days=1)).strftime("%Y%m%d")
+		
+	with open("data/result.txt",'w') as f:
+		json.dump(result, f, indent=4, ensure_ascii=False)
+		
+	with open("data/ssss.txt",'w') as f:
+		json.dump(ssss, f, indent=4, ensure_ascii=False)
+except:
+	print(traceback.format_exc())
+	print(start_date, result, ssss)
--- sub/data.py
+++ sub/data.py
@@ -4,54 +4,54 @@ import requests
 import pandas as pd
 from io import BytesIO
 
-def get_date_stock(date: str = datetime.datetime.today().strftime("%Y%m%d")) -> pd.DataFrame:
-    gen_otp_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
-    gen_otp_data = {
-        'mktId': 'ALL', #ALL: 전체, STK: 코스피, KSQ: 코스닥
-        'trdDd': date,
-        'money': '1',
-        'csvxls_isNo': 'false',
-        'name': 'fileDown',
-        'url': 'dbms/MDC/STAT/standard/MDCSTAT01501'
-    }
-    headers = {'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader'}
-    otp = requests.post(gen_otp_url, gen_otp_data, headers=headers).text
-    down_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
-    down_sector_KS  = requests.post(down_url, {'code':otp}, headers=headers)
-    data = pd.read_csv(BytesIO(down_sector_KS.content), encoding='EUC-KR')
-    if pd.isna(data.loc[0,'시가총액']):
-        yasterday = datetime.datetime.strptime(date, "%Y%m%d") - datetime.timedelta(days=1)
-        return get_date_stock(yasterday.strftime("%Y%m%d"))
-    else: 
-        for col in data.columns:
-            data[col] = data[col].astype('str')
-        return data, date
+def get_date_stock(date: str = datetime.datetime.today().strftime("%Y%m%d"), next_timedelta=1) -> pd.DataFrame:
+	gen_otp_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
+	gen_otp_data = {
+		'mktId': 'ALL', #ALL: 전체, STK: 코스피, KSQ: 코스닥
+		'trdDd': date,
+		'money': '1',
+		'csvxls_isNo': 'false',
+		'name': 'fileDown',
+		'url': 'dbms/MDC/STAT/standard/MDCSTAT01501'
+	}
+	headers = {'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader'}
+	otp = requests.post(gen_otp_url, gen_otp_data, headers=headers).text
+	down_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
+	down_sector_KS  = requests.post(down_url, {'code':otp}, headers=headers)
+	data = pd.read_csv(BytesIO(down_sector_KS.content), encoding='EUC-KR')
+	if pd.isna(data.loc[0,'시가총액']):
+		yasterday = datetime.datetime.strptime(date, "%Y%m%d") + datetime.timedelta(days=next_timedelta)
+		return get_date_stock(yasterday.strftime("%Y%m%d"),next_timedelta=next_timedelta)
+	else: 
+		for col in data.columns:
+			data[col] = data[col].astype('str')
+		return data, date
 
-def start_local_data(date=None) -> dict:
-    if date==None:
-         current_data, date = get_date_stock()
-    else:
-         current_data, date = get_date_stock(date)
-    startdate = date
-    grouped = current_data.groupby('종목코드').agg(list)
-    result = {
-        key: {col: val if col in ['종가', '거래량', '거래대금'] else val[0]
-        for col, val in zip(grouped.columns, values)}
-        for key, values in zip(grouped.index, grouped.values)
-    }
+def start_local_data(date=None, next_timedelta=1) -> dict:
+	if date==None:
+		 current_data, date = get_date_stock(next_timedelta=next_timedelta)
+	else:
+		 current_data, date = get_date_stock(date, next_timedelta)
+	startdate = date
+	grouped = current_data.groupby('종목코드').agg(list)
+	result = {
+		key: {col: val if col in ['거래량', '거래대금'] else val[0]
+		for col, val in zip(grouped.columns, values)}
+		for key, values in zip(grouped.index, grouped.values)
+	}
 
-    for _ in range(9):
-        yasterday = datetime.datetime.strptime(date, "%Y%m%d") - datetime.timedelta(days=1)
-        current_data, date = get_date_stock(yasterday.strftime("%Y%m%d"))
-        for ticker in list(result):
-            indices = current_data.index[current_data['종목코드'] == ticker].tolist()
-            if not indices:
-                result.pop(ticker, None)
-            else:
-                for category in ['종가', '거래량', '거래대금']:
-                    result[ticker][category].append(current_data.loc[indices[0],category])
-    return result, startdate, date
+	for _ in range(10):
+		date = datetime.datetime.strptime(date, "%Y%m%d") + datetime.timedelta(days=-1)
+		current_data, date = get_date_stock(date.strftime("%Y%m%d"), next_timedelta=-1)
+		for ticker in list(result):
+			indices = current_data.index[current_data['종목코드'] == ticker].tolist()
+			if not indices:
+				result.pop(ticker, None)
+			else:
+				for category in ['거래량', '거래대금']:
+					result[ticker][category].append(current_data.loc[indices[0],category])
+	return result, startdate, date
 
 def get_local_data():
-    with open("data\\data.txt",'r') as f:
-        return json.load(f)
+	with open("data\\data.txt",'r') as f:
+		return json.load(f)
