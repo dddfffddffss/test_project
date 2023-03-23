566a5cec69925969b6ba44fc27130f6db4bc0521
dddfffddffss <dlhwadlhwa@gmail.com>
2023-03-24 00:05:36 +0900

    성능개선!

--- main.py
+++ main.py
@@ -12,7 +12,32 @@ import sub.pocket as pocket
 matplotlib.rcParams['font.family'] ='Malgun Gothic'
 matplotlib.rcParams['axes.unicode_minus'] =False
 
-wallet, result={}, []
+wallet, result, ssss={}, [], []
+dh = data.data_handler(start_date = "20180308", analysis_period=10)
+for _ in range(180):
+	current_data, start_date = dh.get_today_data()
+	for ticker in current_data:
+		l = np.array(current_data[ticker]["거래량"]).astype(int)
+		m = np.mean(l)
+		if m==0:
+			continue
+		else:
+			indexed_l = l/m
+			if indexed_l[0]>=5 and (ticker in wallet):
+				denominator = wallet.pop(ticker, None)
+				current = int(current_data[ticker]["시가"])
+				result.append(100*(current-denominator)/denominator)
+			elif np.count_nonzero(indexed_l>=5)>=1:
+				pass
+			elif np.count_nonzero((indexed_l[:-1]-indexed_l[1:])<0)>=8 and (ticker not in wallet):
+				wallet[ticker] = int(current_data[ticker]["종가"])
+
+	men = np.mean(result) if result else 0
+	ssss.append(men)
+	print(start_date, len(result), men)
+	print(len(wallet))
+	print()
+	if _!=179: dh.fetch_next_data()
 
 def make_start_data(date):
     with open("data/"+date+".txt",'w') as f:
@@ -49,37 +74,38 @@ def show_trade_amount():
 	plt.legend(loc=1)
 	plt.show()
 
-ssss = []
-try:
-	start_date = "20180301"
-	for i in range(180):
-		current_data, start_date, end_date = get_web_data(start_date, next_timedelta=1)
-		for ticker in current_data:
-			l = np.array(current_data[ticker]["거래량"]).astype(int)
-			m = np.mean(l)
-			if m==0:
-				continue
-			else:
-				indexed_l = l/m
-				if indexed_l[0]>=3 and (ticker in wallet):
-					denominator = wallet.pop(ticker, None)
-					current = int(current_data[ticker]["시가"])
-					result.append(100*(current-denominator)/denominator)
-				elif np.count_nonzero(indexed_l>=3)>=1:
-					pass
-				elif np.count_nonzero((indexed_l[:-1]-indexed_l[1:])<0)>=8 and (ticker not in wallet):
-					wallet[ticker] = int(current_data[ticker]["종가"])
-		men = np.mean(result) if result else 0
-		ssss.append(men)
-		print(start_date, len(result), men)
-		print()
-		start_date = (datetime.datetime.strptime(start_date, "%Y%m%d") + datetime.timedelta(days=1)).strftime("%Y%m%d")
-		
-	with open("data/result.txt",'w') as f:
-		json.dump(result, f, indent=4, ensure_ascii=False)
-		
-	with open("data/ssss.txt",'w') as f:
-		json.dump(ssss, f, indent=4, ensure_ascii=False)
-except:
-	print(traceback.format_exc())
-	print(start_date, result, ssss)
+def final_star():
+	wallet, result={}, []
+	try:
+		start_date = "20180301"
+		for i in range(180):
+			current_data, start_date, end_date = get_web_data(start_date, next_timedelta=1)
+			for ticker in current_data:
+				l = np.array(current_data[ticker]["거래량"]).astype(int)
+				m = np.mean(l)
+				if m==0:
+					continue
+				else:
+					indexed_l = l/m
+					if indexed_l[0]>=5 and (ticker in wallet):
+						denominator = wallet.pop(ticker, None)
+						current = int(current_data[ticker]["시가"])
+						result.append(100*(current-denominator)/denominator)
+					elif np.count_nonzero(indexed_l>=5)>=1:
+						pass
+					elif np.count_nonzero((indexed_l[:-1]-indexed_l[1:])<0)>=8 and (ticker not in wallet):
+						wallet[ticker] = int(current_data[ticker]["종가"])
+			men = np.mean(result) if result else 0
+			ssss.append(men)
+			print(start_date, len(result), men)
+			print()
+			start_date = (datetime.datetime.strptime(start_date, "%Y%m%d") + datetime.timedelta(days=1)).strftime("%Y%m%d")
+			
+		with open("data/result.txt",'w') as f:
+			json.dump(result, f, indent=4, ensure_ascii=False)
+			
+		with open("data/ssss.txt",'w') as f:
+			json.dump(ssss, f, indent=4, ensure_ascii=False)
+	except:
+		print(traceback.format_exc())
+		print(start_date, result, ssss)
--- sub/data.py
+++ sub/data.py
@@ -7,7 +7,7 @@ from io import BytesIO
 def get_date_stock(date: str = datetime.datetime.today().strftime("%Y%m%d"), next_timedelta=1) -> pd.DataFrame:
 	gen_otp_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
 	gen_otp_data = {
-		'mktId': 'ALL', #ALL: 전체, STK: 코스피, KSQ: 코스닥
+		'mktId': 'KSQ', #ALL: 전체, STK: 코스피, KSQ: 코스닥
 		'trdDd': date,
 		'money': '1',
 		'csvxls_isNo': 'false',
@@ -55,3 +55,61 @@ def start_local_data(date=None, next_timedelta=1) -> dict:
 def get_local_data():
 	with open("data\\data.txt",'r') as f:
 		return json.load(f)
+
+class data_handler:
+	def __init__(self, start_date: str, analysis_period: int):
+		self.analysis_period = analysis_period
+		self.start_data, self.start_date = get_date_stock(start_date)
+		self.ticker_list = list(self.start_data['종목코드'])
+		self.trading_volume = { row['종목코드']: [ row['거래량'] ] for index, row in self.start_data.iterrows() }
+		self.transaction_amount = { row['종목코드']: [ row['거래대금'] ] for index, row in self.start_data.iterrows() }
+
+		temp_date = start_date
+		for _ in range(analysis_period):
+			temp_data, temp_date = get_date_stock(date= temp_date, next_timedelta= -1)
+			temp_date = datetime.datetime.strptime(temp_date, "%Y%m%d") + datetime.timedelta(days=-1)
+			temp_date = temp_date.strftime("%Y%m%d")
+			for ticker in list(self.ticker_list):
+				indices = temp_data.index[temp_data['종목코드'] == ticker].tolist()
+				if indices:
+					self.trading_volume[ticker].append(temp_data.loc[indices[0],'거래량'])
+					self.transaction_amount[ticker].append(temp_data.loc[indices[0],'거래대금'])
+				else:
+					self.trading_volume.pop(ticker, None)
+					self.transaction_amount.pop(ticker, None)
+					self.ticker_list.remove(ticker)
+	
+	def get_pointer_date(self):
+		return self.start_date
+	
+	def get_today_data(self):
+		grouped = self.start_data.groupby('종목코드').agg(list)
+		result = {
+			key: {col: val[0]
+			for col, val in zip(grouped.columns, values) }	
+			for key, values in zip(grouped.index, grouped.values)
+		}
+		for ticker in list(result):
+			if ticker not in self.trading_volume:
+				result.pop(ticker, None)
+				continue
+			result[ticker]['거래량'] = list(self.trading_volume[ticker])
+			result[ticker]['거래대금'] = list(self.transaction_amount[ticker])
+
+		return result, self.start_date
+	
+	def fetch_next_data(self):
+		self.start_date = (datetime.datetime.strptime(self.start_date, "%Y%m%d") + datetime.timedelta(days=1)).strftime("%Y%m%d")
+		self.start_data, self.start_date = get_date_stock(self.start_date)
+
+		for ticker in list(self.ticker_list):
+			indices = self.start_data.index[self.start_data['종목코드'] == ticker].tolist()
+			if indices:
+				self.trading_volume[ticker].insert(0, self.start_data.loc[indices[0],'거래량'])
+				self.trading_volume[ticker].pop()
+				self.transaction_amount[ticker].insert(0, self.start_data.loc[indices[0],'거래대금'])
+				self.transaction_amount[ticker].pop()
+			else:
+				self.trading_volume.pop(ticker, None)
+				self.transaction_amount.pop(ticker, None)
+				self.ticker_list.remove(ticker)
